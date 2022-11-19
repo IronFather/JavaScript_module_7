8be80966f3f72a5287499ff0a4c1cc28fc576ea4
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-11-20 00:11:27 +0200

    2

--- JS_7.js
+++ JS_7.js
@@ -501,31 +501,108 @@
 //   );
 //  -------------------------------------------------------------
 
-const vanillaOutput = document.querySelector(".output.vanilla");
-const throttledOutput = document.querySelector(".output.throttled");
-const eventCounter = {
-  vanilla: 0,
-  throttled: 0
-};
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0
+// };
 
-document.addEventListener("scroll", () => {
-  eventCounter.vanilla += 1;
-  vanillaOutput.textContent = eventCounter.vanilla;
-});
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
 
-document.addEventListener(
-  "scroll",
-  _.throttle(() => {
-    eventCounter.throttled += 1;
-    throttledOutput.textContent = eventCounter.throttled;
-  }, 00)
-);
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 00)
+// );
 
+//  3 -------------------------------------------------------------
 
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const debouncedOutput = document.querySelector(".output.debounced");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+//   debounced: 0,
+// };
 
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
+
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
+
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     eventCounter.debounced += 1;
+//     debouncedOutput.textContent = eventCounter.debounced;
+//   }, 300)
+// );
+
+//  4 -------------------------------------------------------------
+// const vanillaOutput = document.querySelector(".output.vanilla");
+// const throttledOutput = document.querySelector(".output.throttled");
+// const trailingOutput = document.querySelector(".output.trailing");
+// const leadingOutput = document.querySelector(".output.leading");
+// const eventCounter = {
+//   vanilla: 0,
+//   throttled: 0,
+//   trailing: 0,
+//   leading: 0
+// };
+
+// Trailing debounce
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(() => {
+//     eventCounter.trailing += 1;
+//     trailingOutput.textContent = eventCounter.trailing;
+//   }, 300)
+// );
+
+// // Leading debounce
+// document.addEventListener(
+//   "scroll",
+//   _.debounce(
+//     () => {
+//       eventCounter.leading += 1;
+//       leadingOutput.textContent = eventCounter.leading;
+//     },
+//     300,
+//     { trailing: false, leading: true }
+//   )
+// );
+
+// document.addEventListener("scroll", () => {
+//   eventCounter.vanilla += 1;
+//   vanillaOutput.textContent = eventCounter.vanilla;
+// });
+
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     eventCounter.throttled += 1;
+//     throttledOutput.textContent = eventCounter.throttled;
+//   }, 300)
+// );
+
+// LAZY LOADING ----------------------------------------
 
 //  -------------------------------------------------------------
-//  -------------------------------------------------------------
-//  -------------------------------------------------------------
-//  -------------------------------------------------------------
+
+
 //  -------------------------------------------------------------
